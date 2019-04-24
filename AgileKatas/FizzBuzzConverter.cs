e3383a2dd2d33084e109d5e513302918eae54abf
Jeka Vasilyev <e.vasilyev@dodopizza.com>
2019-04-24 17:14:28 +0300

    refactor

--- AgileKatas/FizzBuzzConverter.cs
+++ AgileKatas/FizzBuzzConverter.cs
@@ -4,16 +4,7 @@ namespace AgileKatas
 	{
 		public static string Convert(int i)
 		{
-			switch (i)
-			{
-				case 1:
-					return "1";
-				case 2:
-					return "2";
-				default:
-					return "4";
-				
-			}
+			 return i.ToString();
 		}
 	}
 }
