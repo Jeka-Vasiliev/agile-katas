cb6548d6307c9910b9ba828e40f126921132539c
Jeka Vasilyev <e.vasilyev@dodopizza.com>
2019-05-15 17:01:53 +0300

    convert 4 to IV

--- AgileKatas.Tests/RomanTests.cs
+++ AgileKatas.Tests/RomanTests.cs
@@ -11,6 +11,8 @@ namespace TestProject1
 		[InlineData(2, "II")]
 		[InlineData(5, "V")]
 		[InlineData(6, "VI")]
+		[InlineData(8, "VIII")]
+		[InlineData(4, "IV")]
 		public void Should_convert_arabic_to_roman(int arabicNumber, string expectedRomanNumber)
 		{
 			var romanNumber = RomanConverter.Convert(arabicNumber);
--- AgileKatas/RomanConverter.cs
+++ AgileKatas/RomanConverter.cs
@@ -6,11 +6,16 @@ namespace AgileKatas
 	{
 		public static string Convert(int arabic)
 		{
-			if (arabic >= 5)
-			{
-				return "V" + Convert(arabic - 5);
-			}
+			if (arabic == 4) return "IV";
 			
+			if (arabic < 5) return RomanNumberI(arabic);
+			
+			return "V" + Convert(arabic - 5);
+
+		}
+
+		private static string RomanNumberI(int arabic)
+		{
 			return new string('I', arabic);
 		}
 	}
